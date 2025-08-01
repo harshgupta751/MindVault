9e0f3f91d530243a5f49c39d05a379ee18f1379f
harshgupta751 <harshachieve300@gmail.com>
2025-08-02 04:00:47 +0530

    commit

--- backend/src/validators.ts
+++ backend/src/validators.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const signUpSchema= z.object({
 email: z.string().email({message: "Enter valid email!"}),
 name: z.string().min(3,{message: "Name must be atleast 3 characters!"}).max(50,{message: "Name must be not exceed 50 characters!"}),
-password: z.string().min(6,{message:"Password should be atleast 6 characters!"}).max(8, {message: "Password must not exceed 20 characters!"})
+password: z.string().min(6,{message:"Password should be atleast 6 characters!"}).max(20, {message: "Password must not exceed 20 characters!"})
 })
 
 export const signInSchema= z.object({
