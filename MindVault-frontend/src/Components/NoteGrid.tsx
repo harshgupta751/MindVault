4936ebef25462e6cc4e098895b53880b691835fd
harshgupta751 <harshachieve300@gmail.com>
2025-08-03 04:27:19 +0530

    commit

--- MindVault-frontend/src/Components/NoteGrid.tsx
+++ MindVault-frontend/src/Components/NoteGrid.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import NoteCard from './NoteCard';
 
 interface Note {
@@ -20,6 +20,16 @@ interface NoteGridProps {
 }
 
 const NoteGrid: React.FC<NoteGridProps> = ({ notes, toggleImportant, onDeleteNote }) => {
+
+const endListRef= useRef<HTMLDivElement>(null)
+
+useEffect(()=>{
+if(endListRef.current){
+  endListRef.current.scrollIntoView({behavior: 'smooth'})
+}
+
+},[notes.length])
+
   return (
     <>
     {notes.length==0?
@@ -52,6 +62,7 @@ const NoteGrid: React.FC<NoteGridProps> = ({ notes, toggleImportant, onDeleteNot
           onDelete={onDeleteNote? () => onDeleteNote(note._id) : undefined}
         />
       ))}
+      <div ref={endListRef}></div>
     </div>
 )
     }
